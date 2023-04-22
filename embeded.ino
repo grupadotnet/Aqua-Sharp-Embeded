cb0cd609e18d2c37f375d4922dd36ffdb7c8c618
alien2fg <filipgnojek@gmail.com>
2023-04-22 15:33:06 +0200

    problem_fixed

---
+++ embeded.ino
@@ -0,0 +1,189 @@
+
+//  class CreateMeasurementViewModel()
+// {
+//     public DateTime Time;
+//     public float Temperature;
+//     public uint TDS;
+//     public bool LightOn;
+//     public int AquariumId;
+//     notacja iso rok-miesiac-dzientygodnia-czas
+// }
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+#define PIN 6
+#define NUMPIXELS 30
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+#define DELAYVAL 500
+#define ONE_WIRE_BUS 0
+#define TdsSensorPin A1
+#define VREF 5.0          // analog reference voltage(Volt) of the ADC
+#define SCOUNT 30         // sum of sample point
+int analogBuffer[SCOUNT]; // store the analog value in the array, read from ADC
+int analogBufferTemp[SCOUNT];
+int analogBufferIndex = 0, copyIndex = 0;
+float averageVoltage = 0, tdsValue = 0, temperature = 25;
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+#define SensorPin 0         // pH meter Analog output to Arduino Analog Input 0
+unsigned long int avgValue; // Store the average value of the sensor feedback
+float b;
+int buf[10], temp;
+
+void setup()
+{
+  Serial.begin(9600);
+  sensors.begin();
+
+  pixels.begin();
+  pixels.clear();
+
+  for (int i = 0; i < 2; i++)
+  {
+    if (i == 0)
+    {
+      pixels.fill(pixels.Color(255, 0, 0));
+      pixels.show();
+      delay(500);
+    }
+    else
+    {
+      pixels.fill(pixels.Color(0, 0, 255));
+      pixels.show();
+      delay(500);
+    }
+  }
+
+  pinMode(TdsSensorPin, INPUT);
+  pinMode(2, OUTPUT);
+}
+
+void loop()
+{
+  sensors.requestTemperatures();
+  Serial.print(sensors.getTempCByIndex(0));
+  Serial.println("C");
+  delay(1000);
+  pixels.clear();
+  for (int i = 0; i < 3; i++)
+  {
+    if (i == 0)
+    {
+      pixels.fill(pixels.Color(255, 0, 0));
+      pixels.show();
+      delay(500);
+    }
+    else if (i == 1)
+    {
+      pixels.fill(pixels.Color(0, 255, 0));
+      pixels.show();
+      delay(500);
+    }
+    else
+    {
+      pixels.fill(pixels.Color(0, 0, 255));
+      pixels.show();
+      delay(500);
+    }
+  }
+  static unsigned long analogSampleTimepoint = millis();
+  if (millis() - analogSampleTimepoint > 40U) // every 40 milliseconds,read the analog value from the ADC
+  {
+    analogSampleTimepoint = millis();
+    analogBuffer[analogBufferIndex] = analogRead(TdsSensorPin); // read the analog value and store into the buffer
+    analogBufferIndex++;
+    if (analogBufferIndex == SCOUNT)
+      analogBufferIndex = 0;
+  }
+  static unsigned long printTimepoint = millis();
+  if (millis() - printTimepoint > 800U)
+  {
+    printTimepoint = millis();
+    for (copyIndex = 0; copyIndex < SCOUNT; copyIndex++)
+      analogBufferTemp[copyIndex] = analogBuffer[copyIndex];
+    averageVoltage = getMedianNum(analogBufferTemp, SCOUNT) * (float)VREF / 1024.0;                                                                                                  // read the analog value more stable by the median filtering algorithm, and convert to voltage value
+    float compensationCoefficient = 1.0 + 0.02 * (temperature - 25.0);                                                                                                               // temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0));
+    float compensationVolatge = averageVoltage / compensationCoefficient;                                                                                                            // temperature compensation
+    tdsValue = (133.42 * compensationVolatge * compensationVolatge * compensationVolatge - 255.86 * compensationVolatge * compensationVolatge + 857.39 * compensationVolatge) * 0.5; // convert voltage value to tds value
+    // Serial.print("voltage:");
+    // Serial.print(averageVoltage,2);
+    // Serial.print("V   ");
+    Serial.print("TDS Value:");
+    Serial.print(tdsValue, 0);
+    Serial.println("ppm");
+  }
+
+  for (int i = 0; i < 10; i++) // Get 10 sample value from the sensor for smooth the value
+  {
+    buf[i] = analogRead(SensorPin);
+    delay(10);
+  }
+  for (int i = 0; i < 9; i++) // sort the analog from small to large
+  {
+    for (int j = i + 1; j < 10; j++)
+    {
+      if (buf[i] > buf[j])
+      {
+        temp = buf[i];
+        buf[i] = buf[j];
+        buf[j] = temp;
+      }
+    }
+  }
+  avgValue = 0;
+  for (int i = 2; i < 8; i++) // take the average value of 6 center sample
+    avgValue += buf[i];
+  float phValue = (float)avgValue * 7.0 / 1024 / 6; // convert the analog into millivolt // zmiana wzgledem oryginalu: 5.0 -> 7.0
+  phValue = 2.8 * phValue;                          // convert the millivolt into pH value
+  Serial.print(" pH:");
+  Serial.print(phValue, 2);
+  Serial.println(" ");
+  digitalWrite(13, HIGH);
+  delay(800);
+  digitalWrite(13, LOW);
+}
+int getMedianNum(int bArray[], int iFilterLen)
+{
+  int bTab[iFilterLen];
+  for (byte i = 0; i < iFilterLen; i++)
+    bTab[i] = bArray[i];
+  int i, j, bTemp;
+  for (j = 0; j < iFilterLen - 1; j++)
+  {
+    for (i = 0; i < iFilterLen - j - 1; i++)
+    {
+      if (bTab[i] > bTab[i + 1])
+      {
+        bTemp = bTab[i];
+        bTab[i] = bTab[i + 1];
+        bTab[i + 1] = bTemp;
+      }
+    }
+  }
+  if ((iFilterLen & 1) > 0)
+    bTemp = bTab[(iFilterLen - 1) / 2];
+  else
+    bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
+  return bTemp;
+  for (int i = 0; i < 10; i++) // Get 10 sample value from the sensor for smooth the value
+  {
+    buf[i] = analogRead(SensorPin);
+    delay(10);
+  }
+  for (int i = 0; i < 9; i++) // sort the analog from small to large
+  {
+    for (int j = i + 1; j < 10; j++)
+    {
+      if (buf[i] > buf[j])
+      {
+        temp = buf[i];
+        buf[i] = buf[j];
+        buf[j] = temp;
+      }
+    }
+  }
+}
