924a411065ae451f41291e6a62b9490232899124
alien2fg <filipgnojek@gmail.com>
2023-04-23 01:09:08 +0200

    adding example of sunrise program

---
+++ sunrise_example.ino
@@ -0,0 +1,139 @@
+// //#include "FastLED.h"
+
+// // How many leds in your strip?
+
+// #define NUM_LEDS 38
+
+// // For led chips like Neopixels, which have a data line, ground, and power, you just
+
+// // need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
+
+// // ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
+
+// #define DATA_PIN 13
+
+// #define CLOCK_PIN 12
+
+// DEFINE_GRADIENT_PALETTE( sun_rise ) {
+//   0,     0,  0,  0,   //black
+// 128,   255,  0,  0,   //red
+// 224,   255,255,  0,   //bright yellow
+// 255,   255,255,255 }; //full white
+
+// DEFINE_GRADIENT_PALETTE( sunset ) {
+// 255,   255,255,255,
+// 224,  255,  255, 0,
+// 128, 255, 0, 0,
+// 0,0,0,0};
+
+// // Define the array of leds
+
+// CRGB leds[NUM_LEDS];
+
+// void setup() {
+
+//       // Uncomment/edit one of the following lines for your leds arrangement.
+
+//       // FastLED.addLeds<TM1803, DATA_PIN, RGB>(leds, NUM_LEDS);
+
+//       // FastLED.addLeds<TM1804, DATA_PIN, RGB>(leds, NUM_LEDS);
+
+//       // FastLED.addLeds<TM1809, DATA_PIN, RGB>(leds, NUM_LEDS);
+
+//       // FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
+
+//       // FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
+
+//       // FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
+
+//       FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+
+//       // FastLED.addLeds<APA104, DATA_PIN, RGB>(leds, NUM_LEDS);
+
+//       // FastLED.addLeds<UCS1903, DATA_PIN, RGB>(leds, NUM_LEDS);
+
+//       // FastLED.addLeds<UCS1903B, DATA_PIN, RGB>(leds, NUM_LEDS);
+
+//       // FastLED.addLeds<GW6205, DATA_PIN, RGB>(leds, NUM_LEDS);
+
+//       // FastLED.addLeds<GW6205_400, DATA_PIN, RGB>(leds, NUM_LEDS);
+
+//       // FastLED.addLeds<WS2801, RGB>(leds, NUM_LEDS);
+
+//       // FastLED.addLeds<SM16716, RGB>(leds, NUM_LEDS);
+
+//       // FastLED.addLeds<LPD8806, RGB>(leds, NUM_LEDS);
+
+//       // FastLED.addLeds<P9813, RGB>(leds, NUM_LEDS);
+
+//       // FastLED.addLeds<APA102, RGB>(leds, NUM_LEDS);
+
+//       // FastLED.addLeds<DOTSTAR, RGB>(leds, NUM_LEDS);
+
+//       // FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+
+//       // FastLED.addLeds<SM16716, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+
+//       // FastLED.addLeds<LPD8806, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+
+//       // FastLED.addLeds<P9813, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+
+//       // FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+
+//       // FastLED.addLeds<DOTSTAR, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+
+// }
+
+// void loop() {
+
+//   sunrise();
+
+//   FastLED.show();
+
+// }
+
+// void sunrise() {
+
+//   // total sunrise length, in minutes
+
+//   static const uint8_t sunriseLength = 60;
+
+//   // how often (in seconds) should the heat color increase?
+
+//   // for the default of 30 minutes, this should be about every 7 seconds
+
+//   // 7 seconds x 256 gradient steps = 1,792 seconds = ~30 minutes
+
+//   static const uint8_t interval = (sunriseLength * 60) / 256;
+
+//   // current gradient palette color index
+
+//   static uint8_t heatIndex = 0; // start out at 0
+
+//   // HeatColors_p is a gradient palette built in to FastLED
+
+//   // that fades from black to red, orange, yellow, white
+
+//   // feel free to use another palette or define your own custom one  HeatColors_p  ColorFromPalette(HeatColors_p, heatIndex ); RainbowColors_p
+
+//   CRGB color = ColorFromPalette(HeatColors_p, heatIndex );
+
+//   // fill the entire strip with the current color
+
+//   fill_solid(leds, NUM_LEDS, color);
+
+//   // slowly increase the heat
+
+//   EVERY_N_SECONDS(interval) {
+
+//     // stop incrementing at 255, we don't want to overflow back to 0
+
+//     if(heatIndex < 240) {
+
+//       heatIndex++;
+
+//     }
+
+//   }
+
+// }
